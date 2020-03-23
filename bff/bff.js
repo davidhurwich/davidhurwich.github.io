4a44b4ca34cd2494e3211561b60ba13855a7a814
David Hurwich <davidhurwich@gmail.com>
2020-03-23 15:48:34 -0700

    asd
    s

--- bff/bff.js
+++ bff/bff.js
@@ -2,13 +2,9 @@
 
 function readFile() {
 
-  jQuery.get('test.txt', function(data) {
+  jQuery.get('https://data.bff.fm
+/nowplaying.txt', function(data) {
      console.log(data);
   });
 };
 
-    
-function test(){
-    console.log('test function has been run');
-}    
-    
