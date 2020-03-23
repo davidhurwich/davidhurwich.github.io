dc6302b115798431c2a48efec7cd99ff3e6d5ef6
David Hurwich <davidhurwich@gmail.com>
2020-03-23 15:12:00 -0700

    Asd
    asd

---
+++ bff/bff.js
@@ -0,0 +1,14 @@
+
+
+function readFile() {
+
+  jQuery.get('test.txt', function(data) {
+     console.log(data);
+  });
+};
+
+    
+function test(){
+    console.log('test has been run');
+}    
+    
