14626e5a377610ef4fd8daaffdc588b003941d71
David Hurwich <davidhurwich@gmail.com>
2020-04-08 11:23:08 -0700

    afd
    asdf

---
+++ Image-P5/editor.js
@@ -0,0 +1,15 @@
+let img;
+
+function preload() {
+  img = loadImage('warhol.jpg');
+}
+
+function setup(){
+    createCanvas(500,500);
+    background(200);
+    image(img, 0, 0);
+}
+
+function draw(){
+    
+}
