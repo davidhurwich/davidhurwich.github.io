6388ccb5fcbd470cea228b5dcf0219059a2cd9a5
David Hurwich <davidhurwich@gmail.com>
2020-06-06 12:08:00 -0700

    asd
    asd

---
+++ P5-Game/libraries/main.js
@@ -0,0 +1,16 @@
+let character;
+
+function preload() {
+    character = loadAnimation('character01.png', 'character05.png');
+
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(200);
+
+}
+
+function draw() {
+
+}
